01f5738868bf8a9ea020948c9c51fd18def534bb
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-09-12 08:52:02 +0000

    update

--- src/screens/OnboardingScreen.js
+++ src/screens/OnboardingScreen.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Onboarding from "react-native-onboarding-swiper";
 import {StyleSheet, SafeAreaView, Text, View} from "react-native";
+import {Animated, Easing} from "react-native";
+
 import LottieView from "lottie-react-native";
 
 const OnboardingScreen = ({navigation}) => {
 	const handleDone = () => {
 		navigation.navigate("Home");
 	};
+
 	return (
 		<View className="flex-1">
 			<Onboarding
@@ -17,8 +20,9 @@ const OnboardingScreen = ({navigation}) => {
 					{
 						backgroundColor: "#fff",
 						image: (
-							<View style={styles.lottie}>
+							<View className="">
 								<LottieView
+									className="w-32 h-32"
 									source={require("../../assets/animation/1.json")}
 									autoPlay
 									loop
@@ -31,8 +35,9 @@ const OnboardingScreen = ({navigation}) => {
 					{
 						backgroundColor: "#fff",
 						image: (
-							<View style={styles.lottie}>
+							<View className="flex">
 								<LottieView
+									className="w-96 h-96"
 									source={require("../../assets/animation/3.json")}
 									autoPlay
 									loop
@@ -46,8 +51,9 @@ const OnboardingScreen = ({navigation}) => {
 					{
 						backgroundColor: "#fff",
 						image: (
-							<View style={styles.lottie}>
+							<View className="flex">
 								<LottieView
+									className="w-96 h-96"
 									source={require("../../assets/animation/2.json")}
 									autoPlay
 									loop
@@ -62,12 +68,5 @@ const OnboardingScreen = ({navigation}) => {
 		</View>
 	);
 };
-
-const styles = StyleSheet.create({
-	lottie: {
-		height: 300,
-		width: 400,
-	},
-});
 
 export default OnboardingScreen;
